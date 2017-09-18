c1a1750c06e82fd28e21edaee01a3c8b4f0d4da9
Wiley Wu <wiley12555@gmail.com>
2017-09-18 18:19:32 -0400

    Lesson3 BinaryTree

---
+++ src/code/lint/binaryTreeDivideConquer/three/SubtreeWithMaximumAverage.java
@@ -0,0 +1,108 @@
+package code.lint.binaryTreeDivideConquer.three;
+
+/**
+ * Created by 505007855 on 9/18/2017.
+ */
+public class SubtreeWithMaximumAverage {
+
+
+    
+
+
+
+    // JIU Zhang's Answer
+
+    /*
+    *
+
+    version 1: Traverse + Divide Conquer
+        public class Solution {
+    private class ResultType {
+        public int sum, size;
+        public ResultType(int sum, int size) {
+            this.sum = sum;
+            this.size = size;
+        }
+    }
+
+    private TreeNode subtree = null;
+    private ResultType subtreeResult = null;
+
+
+     @param root the root of binary tree
+     @return the root of the maximum average of subtree
+
+    public TreeNode findSubtree2(TreeNode root) {
+        helper(root);
+        return subtree;
+    }
+
+    private ResultType helper(TreeNode root) {
+        if (root == null) {
+            return new ResultType(0, 0);
+        }
+
+        ResultType left = helper(root.left);
+        ResultType right = helper(root.right);
+        ResultType result = new ResultType(
+                left.sum + right.sum + root.val,
+                left.size + right.size + 1
+        );
+
+        if (subtree == null ||
+                subtreeResult.sum * result.size < result.sum * subtreeResult.size
+                ) {
+            subtree = root;
+            subtreeResult = result;
+        }
+        return result;
+    }
+}
+    *
+   */
+
+
+
+    // Shuo Chang's Answer
+
+    /*
+    *
+    class ResultType{
+    int sum;
+    int num;
+    public ResultType(int sum, int num){
+        this.sum = sum;
+        this.num = num;
+    }
+}
+    public class Solution {
+
+    @param root the root of binary tree
+   @return the root of the maximum average of subtree
+
+    TreeNode node;
+    ResultType max;
+    public TreeNode findSubtree2(TreeNode root) {
+        helper(root);
+        return node;
+    }
+
+    public ResultType helper(TreeNode root){
+        if (root == null){
+            return new ResultType(0, 0);
+        }
+
+        ResultType left = helper(root.left);
+        ResultType right = helper(root.right);
+
+        ResultType curt = new ResultType(left.sum + right.sum + root.val, left.num + right.num + 1);
+        if (node == null || curt.sum * max.num > curt.num * max.sum){
+            node = root;
+            max = curt;
+        }
+        return curt;
+     }
+    *
+    * */
+
+}
