beaf7156a5445b88108cb53de77a345a4fdbfa0a
Wiley Wu <wiley12555@gmail.com>
2017-09-25 17:47:27 -0400

    Lesson 3

--- src/code/lint/binaryTreeDivideConquer/three/MaximumDepthOfBinaryTree.java
+++ src/code/lint/binaryTreeDivideConquer/three/MaximumDepthOfBinaryTree.java
@@ -26,6 +26,29 @@ The maximum depth is 3.
 public class MaximumDepthOfBinaryTree {
 
 
+        /**
+         * @param root: The root of binary tree.
+         * @return: An integer.
+         */
+        public int maxDepth(TreeNode root) {
+            // write your code here
+
+            if(root == null){
+                return 0;
+            }
+
+            int leftPath = 1 + maxDepth(root.left);
+            int rightPath = 1 + maxDepth(root.right);
+
+            if(leftPath < rightPath){
+                return leftPath;
+            }else{
+                return rightPath;
+            }
+
+        }
+
+
     //Jiu Zhang's Answer
 
     // version 1: Divide & Conquer
