b61d2d592168e001ad7cfd0bae5d4ff8eff5f1e1
Wiley Wu <wiley12555@gmail.com>
2017-12-11 13:46:00 -0500

    Lesson 2

--- src/code/lint/binaryTreeDivideConquer/three/SubtreeWithMaximumAverage.java
+++ src/code/lint/binaryTreeDivideConquer/three/SubtreeWithMaximumAverage.java
@@ -4,6 +4,26 @@ package code.lint.binaryTreeDivideConquer.three;
  * Created by 505007855 on 9/18/2017.
  */
 
+/*
+*Given a binary tree, find the subtree with maximum average. Return the root of the subtree.
+
+ Notice
+
+LintCode will print the subtree which root is your return node.
+It's guaranteed that there is only one subtree with maximum average.
+
+Have you met this question in a real interview? Yes
+Example
+Given a binary tree:
+
+     1
+   /   \
+ -5     11
+ / \   /  \
+1   2 4    -2
+return the node 11.
+* */
+
 public class SubtreeWithMaximumAverage {
 
     private class ResultType{
