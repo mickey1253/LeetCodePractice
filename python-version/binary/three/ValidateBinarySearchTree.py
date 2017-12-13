ed44e8cbab565dd895ccd5e22ef94e6c8d1ac274
Wiley Wu <wiley12555@gmail.com>
2017-12-13 15:36:25 -0500

    Lesson 3

---
+++ python-version/binary/three/ValidateBinarySearchTree.py
@@ -0,0 +1,29 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        this.val = val
+        this.left, this.right = None, None
+"""
+
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: True if the binary tree is BST, or false
+    """
+    def isValidBST(self, root):
+        self.lastVal = None
+        self.isBST = True
+        self.validate(root)
+        return self.isBST
+
+    def validate(self, root):
+        if root is None:
+            return
+        self.validate(root.left)
+        if self.lastVal is not None and self.lastVal >= root.val:
+            self.isBST = False
+            return
+        self.lastVal = root.val
+        self.validate(root.right)
