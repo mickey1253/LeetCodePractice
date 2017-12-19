778ef4a60ea731871493a4c49bb8af463f1b3859
Wiley Wu <wiley12555@gmail.com>
2017-12-19 10:26:18 -0500

    Lesson 3

--- src/code/lint/binaryTreeDivideConquer/three/BinaryTreeInorderTraversal.java
+++ src/code/lint/binaryTreeDivideConquer/three/BinaryTreeInorderTraversal.java
@@ -1,9 +1,11 @@
 package code.lint.binaryTreeDivideConquer.three;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by 505007855 on 9/25/2017.
  */
-public class BinaryTreeInorderTraversal {
 
     /*
     *
@@ -21,8 +23,60 @@ Given binary tree {1,#,2,3},
 
 
 return [1,3,2].
+
+Can you do it without recursion?
+
+给出一棵二叉树,返回其中序遍历.
     *
     * */
+public class BinaryTreeInorderTraversal {
+
+    /*
+    * @param root: A Tree
+    * @return: Inorder in ArrayList which contains node values.
+    */
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        // write your code here
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if(root == null){
+            return null;
+        }
+
+        ArrayList<Integer> left = inorderTraversal(root.left);
+        ArrayList<Integer> right = inorderTraversal(root.right);
+
+        result.addAll(left);
+        result.add(root.val);
+        result.addAll(right);
+
+        return result;
+    }
+
+    /*
+    * @param root: A Tree
+    * @return: Inorder in ArrayList which contains node values.
+    */
+    public ArrayList<Integer> inorderTraversal2(TreeNode root) {
+        // write your code here
+        ArrayList<Integer> result = new ArrayList<>();
+        helper(result, root);
+        return result;
+    }
+
+    public void helper(ArrayList<Integer> result, TreeNode root){
+
+        if(root == null){
+            return;
+        }
+
+        helper(result, root.left);
+        result.add(root.val);
+        helper(result, root.right);
+
+    }
+
 
     // JIU ZHANG's Answer
     /*
