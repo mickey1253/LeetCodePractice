2bb6c20307f75debac0446a3a58823e5baf222c5
Wiley Wu <wiley12555@gmail.com>
2017-12-21 11:06:39 -0500

    Lesson 3

---
+++ src/code/lint/binaryTreeDivideConquer/three/MultiTreeNode.java
@@ -0,0 +1,13 @@
+package code.lint.binaryTreeDivideConquer.three;
+
+import java.util.List;
+
+public class MultiTreeNode {
+
+    int val;
+    List<MultiTreeNode> children;
+
+    MultiTreeNode(int x) {
+        val = x;
+    }
+}
